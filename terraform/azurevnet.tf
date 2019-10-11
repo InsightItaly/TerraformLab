c2a0e6052b19ba3ee7ac0278239b01d71730e8b9
Cappello <Riccardo.Cappello@insight.com>
2019-10-11 16:13:18 +0200

    Add Vnet

--- terraform/azurevnet.tf
+++ terraform/azurevnet.tf
@@ -1,25 +1,24 @@
-/*
-resource "azurerm_network_security_group" "ContosoLabMain" {
-  name                = "ContosoLabMainSecurityGroup"
-  location            = "${azurerm_resource_group.ContosoLabMain.location}"
-  resource_group_name = "${azurerm_resource_group.ContosoLabMain.name}"
+resource "azurerm_network_security_group" "TerraformLabMain" {
+  name                = "TerraformLabMainSecurityGroup"
+  location            = "${azurerm_resource_group.TerraformLabMain.location}"
+  resource_group_name = "${azurerm_resource_group.TerraformLabMain.name}"
 }
 
-resource "azurerm_network_ddos_protection_plan" "ContosoLabMain" {
-  name                = "contosolabmainddospplan"
-  location            = "${azurerm_resource_group.ContosoLabMain.location}"
-  resource_group_name = "${azurerm_resource_group.ContosoLabMain.name}"
+resource "azurerm_network_ddos_protection_plan" "TerraformLabMain" {
+  name                = "TerraformLabmainddospplan"
+  location            = "${azurerm_resource_group.TerraformLabMain.location}"
+  resource_group_name = "${azurerm_resource_group.TerraformLabMain.name}"
 }
 
-resource "azurerm_virtual_network" "ContosoLabMain" {
-  name                = "ContosoLabMainVirtualNetwork"
-  location            = "${azurerm_resource_group.ContosoLabMain.location}"
-  resource_group_name = "${azurerm_resource_group.ContosoLabMain.name}"
+resource "azurerm_virtual_network" "TerraformLabMain" {
+  name                = "TerraformLabMainVirtualNetwork"
+  location            = "${azurerm_resource_group.TerraformLabMain.location}"
+  resource_group_name = "${azurerm_resource_group.TerraformLabMain.name}"
   address_space       = ["192.168.0.0/16"]
   //dns_servers         = ["192.168.0.4", "192.168.0.5"]
 
   ddos_protection_plan {
-    id     = "${azurerm_network_ddos_protection_plan.ContosoLabMain.id}"
+    id     = "${azurerm_network_ddos_protection_plan.TerraformLabMain.id}"
     enable = true
   }
 
@@ -29,29 +28,28 @@ resource "azurerm_virtual_network" "ContosoLabMain" {
   }
 }
 
-resource "azurerm_subnet" "ContosoLabMainSubnet_server" {
+resource "azurerm_subnet" "TerraformLabMainSubnet_server" {
   name                 = "server"
-  resource_group_name  = "${azurerm_resource_group.ContosoLabMain.name}"
-  virtual_network_name = "${azurerm_virtual_network.ContosoLabMain.name}"
+  resource_group_name  = "${azurerm_resource_group.TerraformLabMain.name}"
+  virtual_network_name = "${azurerm_virtual_network.TerraformLabMain.name}"
   address_prefix = "192.168.1.0/24"
 }
 
-resource "azurerm_subnet" "ContosoLabMainSubnet_clients" {
+resource "azurerm_subnet" "TerraformLabMainSubnet_clients" {
   name                 = "clients"
-  resource_group_name  = "${azurerm_resource_group.ContosoLabMain.name}"
-  virtual_network_name = "${azurerm_virtual_network.ContosoLabMain.name}"
+  resource_group_name  = "${azurerm_resource_group.TerraformLabMain.name}"
+  virtual_network_name = "${azurerm_virtual_network.TerraformLabMain.name}"
   address_prefix = "192.168.2.0/24"
 }
 
-resource "azurerm_subnet" "ContosoLabMainSubnet_otherservices" {
+resource "azurerm_subnet" "TerraformLabMainSubnet_otherservices" {
   name                 = "otherservices"
-  resource_group_name  = "${azurerm_resource_group.ContosoLabMain.name}"
-  virtual_network_name = "${azurerm_virtual_network.ContosoLabMain.name}"
+  resource_group_name  = "${azurerm_resource_group.TerraformLabMain.name}"
+  virtual_network_name = "${azurerm_virtual_network.TerraformLabMain.name}"
   address_prefix = "192.168.3.0/24"
 }
 
-resource "azurerm_subnet_network_security_group_association" "ContosoLabMainSGSubnet" {
-  subnet_id                 = "${azurerm_subnet.ContosoLabMainSubnet_otherservices.id}"
-  network_security_group_id = "${azurerm_network_security_group.ContosoLabMain.id}"
+resource "azurerm_subnet_network_security_group_association" "TerraformLabMainSGSubnet" {
+  subnet_id                 = "${azurerm_subnet.TerraformLabMainSubnet_otherservices.id}"
+  network_security_group_id = "${azurerm_network_security_group.TerraformLabMain.id}"
 }
-*/
